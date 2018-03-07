d8b7de2f9978cad795008fa2006adb8ebf1eba33
sakires <34483208+sakires@users.noreply.github.com>
2018-03-07 09:35:32 +0100

    ajout de procedure pour satisfactio et layaute
    ajout de joueurNomant pour gardé trace

---
+++ TP2JAVA-master/ChasseAuxPokemons/src/ChasseAuxPokemons.java
@@ -0,0 +1,35 @@
+import tp1.Joueur;
+import tp1.Pokemon;
+
+public class ChasseAuxPokemons {
+
+	public static void main(String[] args) {
+
+		//creation d'objet
+		/*final String mp1;
+		final String mp2;
+		final String mp3;*/
+		
+		//Elles ne compileront pas car il n'y a plus le booleen "diurne ou nocturne"
+		
+		final Pokemon p1 = new Pokemon("Piplup","EAU",5, true, 50,0,50);
+		final Pokemon p2 = new Pokemon("Rowlet","PLANTE",10, false, 50,0,50);
+		final Pokemon p3 = new Pokemon("Totodile","EAU",8, true, 50,0,50);
+		
+		//Les joueurs
+		
+		final Joueur joueur1 = new Joueur("Durant","Sachat",20,new Pokemon[5]);
+		final Joueur joueur2 = new Joueur("Dupont","Pierre",20, new Pokemon[5]);
+		// Jean capture un pokemon qu'il nomme "Rascal " et le donne à Gabrielle
+		joueur1.capturer(p1);
+
+		System.out.println(p1.getNomDonne()); //Affiche : Rascal
+		
+		//affichage des toString		
+		/*System.out.println(mp1);
+		System.out.println(mp2);
+		System.out.println(mp3);*/
+	}
+	
+
+}
